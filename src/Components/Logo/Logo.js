c0623f5d40bda0cb3cd9ecaaf5fb00ed5ac2595e
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-23 15:17:20 +0100

    Add link functionality to  logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Logo.css'
 
 import image from '../../Assets/images/Intranet.svg';
 
 const logo = (props) => {
   return (
-    <div className={classes.logo}>
-      <img src={image} alt="Logo of the Company" className={classes.logo__image}/>
-    </div>
+    <Link to="/">
+      <div className={classes.logo}>
+        <img src={image} alt="Logo of the Company" className={classes.logo__image}/>
+      </div>
+    </Link>
   );
 };
 
