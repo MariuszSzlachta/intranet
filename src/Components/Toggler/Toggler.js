2ca0008d9db827cbd25b6d9622c18069dca624f8
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-23 20:15:14 +0100

    Add classes switch for toggler

--- src/Components/Toggler/Toggler.js
+++ src/Components/Toggler/Toggler.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 import classes from './Toggler.module.scss';
 
-const toggler = (props) => (
-  <button className={[classes.toggler]} onClick={props.toggle}>
-    <span className={classes.toggler__bar}></span>
-    <span className={classes.toggler__bar}></span>
-    <span className={classes.toggler__bar}></span>
-  </button>
-)
+const toggler = (props) => {
+  let componentClasses = ['toggler'];
+  if (!props.toggled) {
+    componentClasses.push('toggled');
+  } else {
+    componentClasses = componentClasses.filter(el => el === 'toggled' ? null : el);
+  }
+
+  return (
+    <button className={componentClasses.join(' ')} onClick={props.toggle}>
+      <span className={classes.toggler__bar}></span>
+      <span className={classes.toggler__bar}></span>
+      <span className={classes.toggler__bar}></span>
+    </button>
+  )
+}
 
 export default toggler;
