1c378b3eea5442f8412b19cedf547f9135446af5
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-23 14:17:51 +0100

    Create Header component basic skeleton

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './Header.css';
+const Header = (props) => (
+  <Header>
+    <div>Logo</div>
+    <div>Navigation</div>
+    <div>searchbar</div>
+  </Header>
+);
+
+export default Header;
