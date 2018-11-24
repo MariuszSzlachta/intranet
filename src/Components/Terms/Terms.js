75bb663a5e94dfe9a8f64660345caf22e975fe2d
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-24 19:29:16 +0100

    Restyle terms component

--- src/Components/Terms/Terms.js
+++ src/Components/Terms/Terms.js
@@ -4,36 +4,34 @@ import classes from './Terms.module.scss';
 
 const terms = (props) => (
   <article className={classes.terms}>
-    <header className={classes.terms__header}>
-      <h1 className={classes.terms__heading_1}>Terms and Conditions</h1>
-    </header>
+    <h1 className={classes.terms__heading_1}>Terms and Conditions</h1>
     <div className={classes.terms__body} >
       <section className={classes.terms__section} >
         <h2 className={classes.terms__heading_2}>&sect; Lorem</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium dicta eligendi quasi adipisci ratione, modi quas dolorem? Consequatur, aspernatur culpa.</p>
+        <p className={classes.terms__text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium dicta eligendi quasi adipisci ratione, modi quas dolorem? Consequatur, aspernatur culpa.</p>
       </section>
       <section className={classes.terms__section} >
         <h2 className={classes.terms__heading_2}>&sect; Ipsum</h2>
-        <ol>
-          <li>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur magni sapiente placeat, delectus nesciunt nulla at possimus quas vitae deserunt minus maxime eligendi nam nisi! Atque aut officiis explicabo deleniti?</p>
+        <ol className={classes.terms__list}>
+          <li className={classes.terms__listItem}>
+            <p className={classes.terms__text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur magni sapiente placeat, delectus nesciunt nulla at possimus quas vitae deserunt minus maxime eligendi nam nisi! Atque aut officiis explicabo deleniti?</p>
           </li>
-          <li>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sit?</p>
+          <li className={classes.terms__listItem}>
+            <p className={classes.terms__text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, sit?</p>
           </li>
-          <li>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, cupiditate.</p>
+          <li className={classes.terms__listItem}>
+            <p className={classes.terms__text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, cupiditate.</p>
           </li>
-          <li>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <li className={classes.terms__listItem}>
+            <p className={classes.terms__text}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
           </li>
         </ol>
       </section>
 
       <section className={classes.terms__section} >
         <h2 className={classes.terms__heading_2}>&sect; Dolor</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit et aliquam in natus?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat doloribus sequi id, iste veritatis sit eveniet iusto voluptatibus dolor tempore?</p>
+        <p className={classes.terms__text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit et aliquam in natus?</p>
+        <p className={classes.terms__text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat doloribus sequi id, iste veritatis sit eveniet iusto voluptatibus dolor tempore?</p>
       </section>
     </div>
     </article>
